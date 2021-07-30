42b674953a769b4b4cbc69b61e0147ecc955728c
Bixkitts <72874643+Bixkitts@users.noreply.github.com>
2021-07-30 19:46:58 +0200

    added wiring

---
+++ src/Wiring.cpp
@@ -0,0 +1,79 @@
+#include "Wiring.h"
+#include "Chip.h"
+
+Wiring::Wiring(Chip* session)
+{
+	LinkedSession = session;
+}
+Wiring::~Wiring()
+{
+
+}
+
+void Wiring::addOutput(Wiring* wire) //wirings can have whatever as their output so we pass it an enum for that.
+{
+		LinkedWires.Push(wire);
+}
+
+void Wiring::addOutput(Gate* gate) //wirings can have whatever as their output so we pass it an enum for that.
+{
+	LinkedGate = gate;
+}
+
+void Wiring::removeOutput(Wiring* wire)	//Search through this wire's connections to remove the correct output from it's list of outputs
+{
+		for (int i; i = LinkedWires.size; i++)
+		{
+			if (LinkedWires[i] == wire)
+			{
+				LinkedWires.Remove(i);
+			}
+		}
+
+}
+
+void Wiring::removeOutput(Gate* gate)	//Search through this wire's connections to remove the correct output from it's list of outputs
+{
+	LinkedGate = nullptr;
+}
+
+void Wiring::addInput(Wiring* wire)
+{
+	InputWire = wire;
+}
+
+
+void Wiring::addInput(Gate* gate)
+{
+	InputGate = gate;
+}
+
+void Wiring::removeInput()
+{
+	InputWire = nullptr;
+	InputGate = nullptr;
+}
+
+void Wiring::setState(bool s)	//sets the state of this wire but also all those attached
+{
+	state = s;
+	LinkedSession->AmmendGateque(LinkedGate);
+	for (int i = 0; i < LinkedWires.size; i++)
+	{
+		LinkedWires[i]->setState(s);
+	}
+}
+
+void Wiring::toggleState()	//toggles the state of this wire but also all those attached. The state is changed so it adds it's gate to the gateque if it outputs to a gate
+{
+	state = !state;
+	LinkedSession->AmmendGateque(LinkedGate);
+	for (int i = 0; i < LinkedWires.size; i++)
+	{
+		LinkedWires[i]->toggleState();
+	}
+	if (LinkedGate == nullptr && LinkedWires.size == 0)
+	{
+		std::cout << "Loose wire outputting " << state << "\n";
+	}
+}
---
+++ src/Wiring.h
@@ -0,0 +1,36 @@
+#ifndef WIRING
+#define WIRING
+
+#include "BArray.h"
+#include "Enums.h"
+class Gate;
+class Chip;
+
+class Wiring
+{
+public:
+    Chip* LinkedSession = nullptr;
+    Gate* LinkedGate = nullptr; //connected gate to this wire
+    BArray<Wiring*> LinkedWires; //connected wires to pass a charge onto on the same timestep
+    Wiring* InputWire = nullptr; //The wire/gate that inputs into this wire. Needed to be noted so that if this wire is deleted/disconnected it can reference the object before it instead of checking every object in the sim.
+    Gate* InputGate = nullptr;
+    bool state = 0; //remember to optimise booleans to save memory ofc. this is the 1/0 state of the wire
+    //uint32_t UID = -1;  //universal ID; where it lies within the session vector of wirings
+
+    Wiring(Chip* session);
+    ~Wiring();
+
+    void addOutput(Wiring* wire); //function that ammends a gate to this wiring that cares about it's state so it can pass the signal on to them
+    void addOutput(Gate* gate);
+    void removeOutput(Wiring* wire); //function to remove an output gate based on it's UID, meaning we'll need to search it up but that's a non issue as it's not per-tick
+    void removeOutput(Gate* gate);
+    void addInput(Wiring* wire);
+    void addInput(Gate* gate);
+    void removeInput();
+    void setState(bool s);
+    void toggleState();
+    
+};
+
+
+#endif
